388bcf60b12911a950a7d688a940a5775aafd17d
Migrim <sebastian@extended-music.com>
2025-03-24 12:13:07 +0100

    Reverted Changes :skull:

--- Server-Installation/forms/company_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField, IntegerField
-from wtforms.validators import InputRequired, Length, Email, Optional, DataRequired
-
-class CompanyForm(FlaskForm):
-    name = StringField('Company Name', validators=[DataRequired()])
-    kundennummer = StringField('Kundennummer', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit_company = SubmitField('Add Company')
--- Server-Installation/forms/user_forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-class UserForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Enter cool username"})
-    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Enter a secure password"})
-    submit = SubmitField('Add User')
